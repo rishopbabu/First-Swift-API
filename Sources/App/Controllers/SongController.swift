5896dba01e35c4dbae1778cf191c15861cf19f84
Rishop Babu <rishop.babu@optisolbusiness.com>
2024-02-06 21:29:20 +0530

    - Post method is added.

--- Sources/App/Controllers/SongController.swift
+++ Sources/App/Controllers/SongController.swift
@@ -13,11 +13,18 @@ struct SongController: RouteCollection {
     func boot(routes: RoutesBuilder) throws  {
         let songs = routes.grouped("songs")
         songs.get(use: index)
+        songs.post(use: create)
     }
     
     
     func index(req: Request) async throws -> [Song] {
         try await Song.query(on: req.db).all()
+    }
+    
+    func create(req: Request) async throws -> Song {
+        let song = try req.content.decode(Song.self)
+        try await song.save(on: req.db)
+        return song
     }
     
 }
